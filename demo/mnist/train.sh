df0be183c2dd5e2ccb2e9e6c0d883332fd74b7e0
Khokhlov Yuri <khokhlov@speechpro.com>
2023-02-27 03:55:05 +0300

    upd demo/mnist scripts

--- demo/mnist/train.sh
+++ demo/mnist/train.sh
@@ -1,17 +1,33 @@
 #!/bin/env bash
 
-srun \
-  -p gpu \
-  -c 8 \
-  --gres=gpu:1 \
-  --export=PATH,LD_LIBRARY_PATH \
-    ./train.yaml \
-      -s . \
-      -m effnet.yaml \
-      -u lr=0.00064 \
-      -u batch_size=64 \
-      -u num_epochs=10 \
-      -u train_loader.options.num_workers=4 \
-      -u train_loader.options.pin_memory=true \
-      -u valid_loader.options.num_workers=4 \
-      -u valid_loader.options.pin_memory=true
+lr=0.00064
+batch_size=64
+num_epochs=10
+
+train_dir=train_effnet_${lr}_${batch_size}_${num_epochs}
+mkdir -p $train_dir
+cp train.yaml effnet.yaml $train_dir
+script=$train_dir/train.sh
+cat << EOF > $script
+#!/bin/env bash
+srun \\
+  --partition=gpu \\
+  --gres=gpu:1 \\
+  --cpus-per-task=8 \\
+  --export=PATH,LD_LIBRARY_PATH \\
+  ./train.yaml \\
+    -l INFO \\
+    -s . \\
+    -m effnet.yaml \\
+    -u train_dir=$train_dir \\
+    -u lr=$lr \\
+    -u batch_size=$batch_size \\
+    -u num_epochs=$num_epochs \\
+    -u train_loader.options.num_workers=4 \\
+    -u train_loader.options.pin_memory=true \\
+    -u valid_loader.options.num_workers=4 \\
+    -u valid_loader.options.pin_memory=true || exit 1
+EOF
+chmod +x $script
+
+$script |& tee $train_dir/train.log
--- demo/mnist/train_ddp.sh
+++ demo/mnist/train_ddp.sh
@@ -1,21 +1,53 @@
 #!/bin/env bash
 
-num_gpus=3
+#export NCCL_DEBUG=INFO
+#export NCCL_DEBUG_SUBSYS=ALL
+export NCCL_SOCKET_IFNAME=enp94s0f0
+export MASTER_PORT=5518
 
-srun \
-  -p gpu \
-  -c $((8 * num_gpus)) \
-  --gres=gpu:$num_gpus \
-  --export=PATH,LD_LIBRARY_PATH \
-    ./train.yaml \
-      -s . \
-      -m effnet.yaml \
-      -m ddp.yaml \
-      -u devices=$num_gpus \
-      -u lr=0.00064 \
-      -u batch_size=64 \
-      -u num_epochs=10 \
-      -u train_loader.options.num_workers=$((2 * num_gpus)) \
-      -u train_loader.options.pin_memory=true \
-      -u valid_loader.options.num_workers=$((2 * num_gpus)) \
-      -u valid_loader.options.pin_memory=true
+num_nodes=2
+nper_node=2
+lr=0.00064
+batch_size=16
+num_epochs=10
+
+train_dir=train_effnet_${lr}_${batch_size}_${num_epochs}_ddp
+mkdir -p $train_dir
+cp train.yaml effnet.yaml ddp.yaml $train_dir
+script=$train_dir/train.sh
+cat << EOF > $script
+#!/bin/env bash
+srun \\
+  ./train.yaml \\
+    -l INFO \\
+    -s . \\
+    -m effnet.yaml \\
+    -m ddp.yaml \\
+    -u train_dir=$train_dir \\
+    -u num_nodes=$num_nodes \\
+    -u devices=$nper_node \\
+    -u lr=$lr \\
+    -u batch_size=$batch_size \\
+    -u num_epochs=$num_epochs \\
+    -u train_loader.options.num_workers=4 \\
+    -u train_loader.options.pin_memory=true \\
+    -u valid_loader.options.num_workers=4 \\
+    -u valid_loader.options.pin_memory=true \\
+    -u progress.options.refresh_rate=20 || exit 1
+EOF
+chmod +x $script
+
+[ -f $train_dir/train.log ] && rm $train_dir/train.log
+
+sbatch \
+  --exclude=sc14 \
+  --partition=gpu \
+  --cpus-per-task=8 \
+  --nodes=$num_nodes \
+  --gres=gpu:$nper_node \
+  --ntasks-per-node=$nper_node \
+  --export=PATH,LD_LIBRARY_PATH,NCCL_DEBUG,NCCL_DEBUG_SUBSYS,NCCL_SOCKET_IFNAME,MASTER_PORT \
+  --open-mode=append \
+  -e $train_dir/train.log \
+  -o $train_dir/train.log \
+  $script
