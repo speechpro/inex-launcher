94c0b7726e951ebc39d9bebfea1cbf8566804fa3
Khokhlov Yuri <khokhlov@speechpro.com>
2024-10-16 03:05:46 +0300

    add inex.helpers.compose() helper function

---
+++ tests/test_compose.py
@@ -0,0 +1,44 @@
+import unittest
+import tempfile
+from pathlib import Path
+from omegaconf import OmegaConf
+from inex.helpers import compose
+
+
+class TestModule(unittest.TestCase):
+    @staticmethod
+    def test_config_file():
+        path = Path(__file__).with_suffix('.1.yaml')
+        assert path.is_file()
+        config = OmegaConf.load(path)
+        config = OmegaConf.to_container(config)
+        path = Path(__file__).with_suffix('.2.yaml')
+        assert path.is_file()
+        with tempfile.TemporaryDirectory() as tempdir:
+            result_path = Path(tempdir) / 'config.yaml'
+            config = compose(
+                config=config,
+                merge_paths=str(path),
+                merge_dicts=[
+                    {'option4': 4},
+                    {'option5': 5},
+                ],
+                override=[
+                    'option1=1',
+                    'option2=2',
+                    'option3=3',
+                ],
+                result_path=str(result_path),
+            )
+            result = OmegaConf.load(result_path)
+        result = OmegaConf.to_container(result)
+        for i in [1, 2, 3]:
+            assert config[f'option{i}'] == i
+            assert config[f'value{i}']['options']['value'] == i
+        for i in [4, 5]:
+            assert config[f'option{i}'] == i
+        assert result == config
+
+
+if __name__ == '__main__':
+    unittest.main()
